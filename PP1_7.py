8a4527ac823f2deb98de58bd4f414d4aa127b262
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-25 15:31:45 +0000

    noighc

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,42 @@
 
 
-q1():
+def q1():
   #Write code here
+  print(True)
 
-q2():
+def q2():
   #Write code here
+  num = int(input("Input an interger: "))
+  num = num > 5
+  print(num)
 
-q3():
+def q3():
   #Write code here
+  letter = ("Input the letter a: ")
+  letter = "a" == letter
+  print(letter)
 
-q4():
+def q4():
   #Write code here
+  word = input("Input a word earlier in the dictionary than google: ")
+  word = word > "google"
+  print(word)
 
-q5():
+def q5():
   #Write code here
+  num1 = int(input("Input an interger: "))
+  num2 = int(input("Input another interger: "))
+  print(bool(num1*num2 > 40))
+
+
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
